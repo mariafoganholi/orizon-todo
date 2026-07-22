51fc65f88dd1d434d6f9eaac3464eb95e3a8bc63
Maria Eduarda <mariaeduardafcf@hotmail.com>
2026-07-22 11:26:20 -0300

    add endpoint to register a new user

--- apps/api/config/urls.py
+++ apps/api/config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from todos.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', obtain_auth_token, name='api_login'),
+    path('api/register/', RegisterView.as_view(), name='api_register'),
 ]
--- apps/api/todos/views.py
+++ apps/api/todos/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    authentication_classes = []  # public
+    permission_classes = []
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response(
+            {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'token': token.key,
+            },
+            status=status.HTTP_201_CREATED,
+        )
